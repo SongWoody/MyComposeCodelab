adb3607fa5286335fc18d0c1bb0b2ce0a3171abd
Woody <syg1138@gmail.com>
2022-04-27 23:06:24 +0900

    State hoisting

--- app/src/main/java/com/example/mycomposecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/mycomposecodelab/WellnessScreen.kt
@@ -1,9 +1,13 @@
 package com.example.mycomposecodelab
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    var count by remember { mutableStateOf(0) }
+    StatelessCounter(
+        count,
+        { count++ },
+        modifier)
 }
