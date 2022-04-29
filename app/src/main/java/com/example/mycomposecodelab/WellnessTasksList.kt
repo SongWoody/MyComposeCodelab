3d36472e8c20755aab8a4785d1b277c3c0d4965e
Woody <syg1138@gmail.com>
2022-04-29 19:06:07 +0900

    rememberLazyListState 사용

--- app/src/main/java/com/example/mycomposecodelab/WellnessTasksList.kt
+++ app/src/main/java/com/example/mycomposecodelab/WellnessTasksList.kt
@@ -2,6 +2,7 @@ package com.example.mycomposecodelab
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -14,7 +15,8 @@ fun WellnessTaskList(
     list: List<WellnessTask> = remember { getWellnessTasks() }
 ) {
     LazyColumn(
-        modifier = modifier
+        modifier = modifier,
+        state = rememberLazyListState()
     ) {
         items(list) { task ->
             WellnessTaskItem(taskName = task.label)
