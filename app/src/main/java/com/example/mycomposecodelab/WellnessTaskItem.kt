fffed3a9a606d9667e242a3925319ad1d27fb596
Woody <syg1138@gmail.com>
2022-04-29 18:49:25 +0900

    Lazy Column 에서 rememberSaveable 사용

--- app/src/main/java/com/example/mycomposecodelab/WellnessTaskItem.kt
+++ app/src/main/java/com/example/mycomposecodelab/WellnessTaskItem.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -52,7 +53,7 @@ fun WellnessTaskItem(
 fun WellnessTaskItem(
     taskName: String,
     modifier: Modifier = Modifier) {
-    var checkedState  by remember {
+    var checkedState  by rememberSaveable {
         mutableStateOf(false)
     }
 
